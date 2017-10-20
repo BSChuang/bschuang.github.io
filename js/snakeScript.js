73f35f4d2ae116633d2d3554e87122d4d5e04add
Ben Chuang <Chilshifter@users.noreply.github.com>
2017-10-20 00:02:07 -0400

    Snake v1.0.0

--- js/snakeScript.js
+++ js/snakeScript.js
@@ -1,14 +1,17 @@
-var canvas;
+var widthPixels = 0;
+var lengthPixels = 0;
+var stopInterval;
 var score = 0;
 
 var snakeSize = 4;
-var food;
+var dead = false;
+var dir = "d";
+var food = [];
 
 function makeOutline() {
-    var size = 25;
     var pixels = "";
-    var widthPixels = Math.floor(screen.height * 3/4 / 20);
-    var lengthPixels = Math.floor(screen.width * 3.5/4 / 20);
+    widthPixels = Math.floor(screen.height * 3/4 / 20);
+    lengthPixels = Math.floor(screen.width * 3.5/4 / 20);
     for (var i=0; i < widthPixels; i++) {
         pixels += "<div class='row'>"
         for (var j=0; j < lengthPixels; j++) {
@@ -17,7 +20,7 @@ function makeOutline() {
         pixels += "</div>"
     }
 
-    document.getElementById('art').innerHTML += pixels;
+    document.getElementById('art').innerHTML = pixels;
     document.getElementById('art').style.tableLayout = "fixed";
 }
 
@@ -32,9 +35,15 @@ function addZero (num){
 }
 
 function makeSnake() {
+    makeOutline();
+    dead = false;
+    dir = "d"
+    document.getElementById("restart").innerHTML = ""
+    
     snakeH[0] = 7;
     snakeH[1] = 2;
 
+    snakeB = [];
     snakeB.push([6, 2])
     snakeB.push([5, 2])
     snakeB.push([4, 2])
@@ -44,14 +53,26 @@ function makeSnake() {
     document.getElementById('0502').style.backgroundColor="red";
     document.getElementById('0402').style.backgroundColor="red";
     document.getElementById('0302').style.backgroundColor="red";
+
+    makeFood();
+
+    stopInterval = setInterval(Death, 75);
 }
 
-var dir = "d";
-var test = []
+function makeFood (){
+    food = [Math.floor(Math.random() * lengthPixels), Math.floor(Math.random() * widthPixels)]
+    while (snakeB.includes(food) || snakeH.includes(food))
+        food = [Math.floor(Math.random() * lengthPixels), Math.floor(Math.random() * widthPixels)]
+
+    foodS = addZero(food[0]) + addZero(food[1])
+    document.getElementById(foodS).style.backgroundColor="yellow";
+}
+
+
 function update(){
-    var snakeX = snakeH[0];
-    var snakeY = snakeH[1];
-    snakeB.unshift([snakeX, snakeY]);
+    score = snakeB.length + 1
+    document.getElementById("score").innerHTML = "Score: " + score
+    snakeB.unshift([snakeH[0], snakeH[1]]); // Add to array
     if (dir == "r")
         snakeH[0] += 1;
     else if (dir == "l")
@@ -64,7 +85,72 @@ function update(){
     var headPos = addZero(snakeH[0]) + addZero(snakeH[1]) // Sets the next head coord as red
     document.getElementById(headPos).style.backgroundColor="red"
 
-    var tailPosA = snakeB.pop()
-    var tailPos = addZero(tailPosA[0]) + addZero(tailPosA[1]) // Sets the array coord at end of snake body as blue
-    document.getElementById(tailPos).style.backgroundColor="blue"
+     
+    if (!(snakeH[0] == food[0] && snakeH[1] == food[1])){ // Not Hit food
+        var tailPosA = snakeB.pop() // Remove from array
+        var tailPos = addZero(tailPosA[0]) + addZero(tailPosA[1])
+        document.getElementById(tailPos).style.backgroundColor="gray"
+    }
+    else { // Hit food
+        makeFood();
+    }
+
+    pressed = false; // Hold second key press
+    if (nextPress != ""){
+        dir = nextPress;
+        nextPress = "";
+    }
 }
+
+function Death(){
+    var headPosS = addZero(snakeH[0]) + addZero(snakeH[1])
+    for (var i = 0; i < snakeB.length; i++){
+        var posS = addZero(snakeB[i][0]) + addZero(snakeB[i][1])
+        if (posS == headPosS){
+            dead = true;
+        }
+    }
+
+    if ((dir == "l" && snakeH[0] == 0) || (dir == "r" && snakeH[0] == lengthPixels - 1) || (dir == "u" && snakeH[1] == 0) || (dir == "d" && snakeH[1] == widthPixels - 1)){
+        dead = true;
+        document.getElementById("restart").innerHTML = "<button type='button' onclick='makeSnake();'>Restart!</button>"
+    }
+
+    if (dead)
+        clearInterval(stopInterval)
+    else
+        update();
+}
+
+var pressed = false;
+var nextPress = "";
+addEventListener("keydown", function(event){
+    if (pressed) {
+        if (dir != "d" && (event.keyCode == 87 || event.keyCode == 38))
+            nextPress = "u"
+        else if (dir != "u" && (event.keyCode == 83 || event.keyCode == 40))
+            nextPress = "d"
+        else if (dir != "l" && (event.keyCode == 68 || event.keyCode == 39))
+            nextPress = "r"
+        else if (dir != "r" && (event.keyCode == 65 || event.keyCode == 37))
+            nextPress = "l"
+    }
+    else {
+        if (dir != "d" && (event.keyCode == 87 || event.keyCode == 38)){
+            dir = "u"
+            pressed = true;
+        }
+        else if (dir != "u" && (event.keyCode == 83 || event.keyCode == 40)){
+            dir = "d"
+            pressed = true;
+        }
+        else if (dir != "l" && (event.keyCode == 68 || event.keyCode == 39)){
+            dir = "r"
+            pressed = true;
+        }
+        else if (dir != "r" && (event.keyCode == 65 || event.keyCode == 37)){
+            dir = "l"
+            pressed = true;
+        }
+    }
+}, false);
